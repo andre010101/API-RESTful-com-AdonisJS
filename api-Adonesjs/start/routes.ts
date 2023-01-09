4d764eaea07833ce466d7e3c1df02553ddfa934f
Andre Pessoa <andrepessoa182@hotmail.com>
2023-01-08 23:18:40 -0300

    upload de imagens

--- api-Adonesjs/start/routes.ts
+++ api-Adonesjs/start/routes.ts
@@ -24,5 +24,5 @@ Route.group(() => {
   Route.get('/', async () => {
     return { hello: 'world' }
   })
-  Route.post('/moments', 'MomentsController.store')
+  Route.resource('/moments', 'MomentsController').apiOnly()
 }).prefix('/api')
